cbe6398540c2d46c1ae36c651a283d0f4f31c83f
sensui <sensuijunior@hotmail.com>
2020-07-04 22:11:12 -0300

    Project ready to code

---
+++ src/conversor-moedas.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './conversor-moedas.css';
+
+function ConversorMoedas() {
+  return (
+    <h1>Conversor de moedas</h1>
+  );
+}
+
+export default ConversorMoedas;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ConversorMoedas from './conversor-moedas';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ConversorMoedas />
   </React.StrictMode>,
   document.getElementById('root')
 );
